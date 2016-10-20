ff04b9c9482cfaa06173628dfc9301b4dded47ff
Maxym Tovstenko <maxym.t28@gmail.com>
2016-10-20 23:10:17 +0300

    Solution

---
+++ ZigZag-Conversion/src/com/maxt28/Solution.java
@@ -0,0 +1,27 @@
+package com.maxt28;
+
+public class Solution {
+    public String convert(String s, int numRows) {
+        String[] stringArray = new String[numRows];
+        String result = "";
+        int space = (2 * numRows - 2);
+        System.out.println(s.length() + " " + numRows);
+        if (s.length() <= numRows || numRows == 1) {
+            return s;
+        }
+        for (int row = 0; row < numRows; row++) {
+            stringArray[row] = "";
+            stringArray[row] += s.charAt(row);
+            for (int index = space; index - row < s.length(); index += space) {
+                if (row != 0 && (row != numRows - 1)) {
+                    stringArray[row] += s.charAt(index - row);
+                }
+                if (index + row < s.length()) {
+                    stringArray[row] += s.charAt(index + row);
+                }
+            }
+            result += stringArray[row];
+        }
+        return result;
+    }
+}
